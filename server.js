7d9ce957acaa4b1fb27cd9945fdda174d73e71b4
aitzhan <aitzhan.anarbay@mail.ru>
2021-05-15 21:03:47 +0600

    112

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, '.dist/metodichka-angular')));
+app.use(express.static(path.join(__dirname, 'metodichka-angular/dist/demo')));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: '.dist/metodichka-angular'}),
+  res.sendFile('index.html', {root: '.dist/demo'}),
   );
 
 app.listen(process.env.PORT || 8080);
