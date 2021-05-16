138c87dd1e5a6ba974927c546bfa78cc379820e9
aitzhan <aitzhan.anarbay@mail.ru>
2021-05-16 19:53:50 +0600

    changed server.js

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
-app.get('/*', function (req, res) {
-  res.sendFile(`./metodichka-angular/dist/index.html`); });
+// app.get('/*', function (req, res) {
+//   res.sendFile(`./metodichka-angular/dist/index.html`); });
+
+app.get('', function(req, res) {
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'src', 'index.html'));
+});
 console.log('console listening')
