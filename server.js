d79b4645053d9ab4fc9cd5773fd9ec89ea442a15
AitZhan12 <51156370+AitZhan12@users.noreply.github.com>
2021-05-16 19:15:19 +0600

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/metodichka-angular'));
 app.listen(process.env.PORT || 8080);
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html')); });
+  res.sendFile(path.join(__dirname + '/dist/metodichka-angular/index.html')); });
 console.log('console listening')
