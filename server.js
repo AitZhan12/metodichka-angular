fce4ad0dbea86f7b6a5982097fa3f92b1f6b6bca
aitzhan <aitzhan.anarbay@mail.ru>
2021-05-17 17:29:14 +0600

    minor changes

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
+function requireHTTPS(req, res, next) {
+  if (!req.secure && req.get('forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-const PORT = app.listen(process.env.PORT || 8080);
+app.use(requireHTTPS);
+
+app.use(express.static(`./dist/metodichka-angular`));
+const PORT = app.listen(process.env.PORT || 3000);
 // app.get('/*', function (req, res) {
 //   res.sendFile(`./metodichka-angular/dist/index.html`); });
 
@@ -12,8 +20,8 @@ app.get('', function(req, res) {
   res.sendFile(path.join(__dirname, 'src', 'index.html'));
 });
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'src', 'index.html'));
+app.get(`/*`, function(req, res) {
+  res.sendFile(`index.html`, {root: 'dist/metodichka-angular'});
 });
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
 
